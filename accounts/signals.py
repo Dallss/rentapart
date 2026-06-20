a5cc8e6cce4ca8bad8efa64c3ce872d6d7f59816
Dallss <raalquicer@up.edu.ph>
2026-06-21 06:44:53 +0800

    merge with prev

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,5 +6,4 @@ from .models import User, Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        print('sd')
         Profile.objects.get_or_create(user=instance)
